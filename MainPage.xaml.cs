964b73219cc6b5c7fe25c403dc0078039d09882e
gdemartino93 <g.demartino9393@gmail.com>
2025-02-18 00:00:04 +0100

    generate random color and avoid Slider_ValueChanged get invoked on generate random color

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,6 +2,7 @@
 {
     public partial class MainPage : ContentPage
     {
+        private bool IsRandom;
         public MainPage()
         {
             InitializeComponent();
@@ -9,12 +10,15 @@
 
         private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
         {
-            var red = sldRed.Value;
-            var green = sldGren.Value;
-            var blue = sldBlue.Value;
+            if (!IsRandom)
+            {
+                var red = sldRed.Value;
+                var green = sldGreen.Value;
+                var blue = sldBlue.Value;
 
-            Color color = Color.FromRgb(red, green, blue);
-            SetColor(color);
+                Color color = Color.FromRgb(red, green, blue);
+                SetColor(color);
+            }
         }
 
         private void SetColor(Color color)
@@ -26,7 +30,20 @@
 
         private void btnRandom_Clicked(object sender, EventArgs e)
         {
+            IsRandom = true;
+            Random random = new Random();
+            Color color = new Color(
+                random.Next(0,256),
+                random.Next(0,256),
+                random.Next(0,256));
 
+            SetColor(color);
+
+            sldRed.Value = color.Red;
+            sldBlue.Value = color.Blue;
+            sldGreen.Value = color.Green;
+
+            IsRandom = false;
         }
     }
 
