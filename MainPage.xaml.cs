9cf3fdf67bb0ba84b86e12f756821ebe8418af51
gdemartino93 <g.demartino9393@gmail.com>
2025-02-17 23:49:04 +0100

    set color on slider value changed

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -6,6 +6,28 @@
         {
             InitializeComponent();
         }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var red = sldRed.Value;
+            var green = sldGren.Value;
+            var blue = sldBlue.Value;
+
+            Color color = Color.FromRgb(red, green, blue);
+            SetColor(color);
+        }
+
+        private void SetColor(Color color)
+        {
+            Container.BackgroundColor = color;
+            btnRandom.BackgroundColor = color;
+            lblHexCode.Text = color.ToHex();
+        }
+
+        private void btnRandom_Clicked(object sender, EventArgs e)
+        {
+
+        }
     }
 
 }
