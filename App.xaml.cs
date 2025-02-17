eb04fd707bab8ec4fc7dc6d7bd3160cfbd0b6f93
gdemartino93 <g.demartino9393@gmail.com>
2025-02-17 21:03:46 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MAUI_ColorMaker
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
